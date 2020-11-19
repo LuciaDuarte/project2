790dab6c996b33f415d063dc19078ebceab04296
Lucia Duarte <analuciacfduarte@gmail.com>
2020-11-19 18:14:02 +0000

    forces change

--- routes/mapRoute.js
+++ routes/mapRoute.js
@@ -1,47 +1,48 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const User = require('../models/User');
 
-function requireLogin(req, res, next) {  // we create a middleware function, to pass it in the route.
-  if (req.session.currentUser) {   // If this exists, means we are authenticated.
+function requireLogin(req, res, next) {
+  // we create a middleware function, to pass it in the route.
+  if (req.session.currentUser) {
+    // If this exists, means we are authenticated.
     next();
   } else {
-    res.redirect('/login')
+    res.redirect('/login');
   }
 }
 
 // GET THE HELPER MAP
-router.get('/h-map', (req, res) =>{
-  User.find({userType: "needy"})
-  .populate("needType")
-  .then ((needyUsersFromDB) => {
-    let needyUsersFromDBString = JSON.stringify(needyUsersFromDB);
-    let userStr = JSON.stringify(req.session.currentUser);
-    console.log("userstr", userStr);
-    res.render('map/helper-map', {user: userStr, needyUsersFromDB: needyUsersFromDB, markers: needyUsersFromDBString}); // map shall render also the object: helperNearby:
-  })
-  .catch((err) => console.log("Error:", err))    
-})
-
-
-
+router.get('/h-map', (req, res) => {
+  User.find({ userType: 'needy' })
+    .populate('needType')
+    .then(needyUsersFromDB => {
+      let needyUsersFromDBString = JSON.stringify(needyUsersFromDB);
+      let userStr = JSON.stringify(req.session.currentUser);
+      console.log('userstr', userStr);
+      res.render('map/helper-map', {
+        user: userStr,
+        needyUsersFromDB: needyUsersFromDB,
+        markers: needyUsersFromDBString
+      }); // map shall render also the object: helperNearby:
+    })
+    .catch(err => console.log('Error:', err));
+});
 
 // GET THE NEEDY MAP
-router.get('/n-map', (req, res) =>{
-  User.find({userType: "helper"})
-  .populate("serviceType")
-  .then ((helpersUsersFromDB) => {
-    let helpersUsersFromDBString = JSON.stringify(helpersUsersFromDB);
-    let userStr = JSON.stringify(req.session.currentUser);
-    res.render('map/needy-map', {user: userStr, helpersUsersFromDB: helpersUsersFromDB, markers: helpersUsersFromDBString});
-  })
- .catch((err) => console.log("Error:", err))    
-})
-
+router.get('/n-map', (req, res) => {
+  User.find({ userType: 'helper' })
+    .populate('serviceType')
+    .then(helpersUsersFromDB => {
+      let helpersUsersFromDBString = JSON.stringify(helpersUsersFromDB);
+      let userStr = JSON.stringify(req.session.currentUser);
+      res.render('map/needy-map', {
+        user: userStr,
+        helpersUsersFromDB: helpersUsersFromDB,
+        markers: helpersUsersFromDBString
+      });
+    })
+    .catch(err => console.log('Error:', err));
+});
 
 module.exports = router;
-
-
-
- 
-
